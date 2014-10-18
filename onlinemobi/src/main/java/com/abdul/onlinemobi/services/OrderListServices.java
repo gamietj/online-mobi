c4b7c0836feb583687bcd0d4cec4cea7badf5525
Lucrecia <Lucrecia@Lucrecia-PC.ADS.CPUT.AC.ZA>
2014-10-18 13:10:48 +0200

    files added into services

---
+++ onlinemobi/src/main/java/com/abdul/onlinemobi/services/OrderListServices.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.abdul.onlinemobi.services;
+
+import com.abdul.onlinemobi.domain.Orders;
+import java.util.List;
+
+/**
+ *
+ * @author Lucrecia
+ */
+public interface OrderListServices {
+        public List<Orders>getAllOrders();
+
+}
